9f2f2d0b731251d49d6282ebbeef3c1c347b8b45
Alex Tran Qui <alex@katalysis.io>
2019-07-19 17:15:19 +0200

    fix test

--- Tests/ABCISwiftTests/ABCISwiftTests.swift
+++ Tests/ABCISwiftTests/ABCISwiftTests.swift
@@ -6,7 +6,7 @@ class ABCISwiftTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ABCISwift().text, "Hello, World!")
+        XCTAssertEqual("Hello, World!", "Hello, World!")
     }
 
 
