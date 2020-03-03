316a6e625665a8ca03a4ceaf59a9853f908f8e85
Alex Tran Qui <alex@katalysis.io>
2020-03-03 16:20:52 +0100

    more name change

--- scripts/generate_contributors_list.sh
+++ scripts/generate_contributors_list.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ##===----------------------------------------------------------------------===##
 ##
-## This source file is part of the ABCISwift open source project
+## This source file is part of the CosmosSwift/ABCI open source project
 ##
-## Copyright (c) 2019 ABCISwift project authors
+## Copyright (c) 2019 CosmosSwift/ABCI project authors
 ## Licensed under Apache License v2.0
 ##
 ## See LICENSE.txt for license information
-## See CONTRIBUTORS.txt for the list of ABCISwift project authors
+## See CONTRIBUTORS.txt for the list of CosmosSwift/ABCI project authors
 ##
 ## SPDX-License-Identifier: Apache-2.0
 ##
@@ -32,11 +32,12 @@ contributors=$( cd "$here"/.. && git shortlog -es | cut -f2 | sed 's/^/- /' )
 
 cat > "$here/../CONTRIBUTORS.txt" <<- EOF
 	For the purpose of tracking copyright, this is the list of individuals and
-	organizations who have contributed source code to ABCISwift.
+	organizations who have contributed source code to CosmosSwift/ABCI.
 	For employees of an organization/company where the copyright of work done
 	by employees of that company is held by the company itself, only the company
 	needs to be listed here.
 	## COPYRIGHT HOLDERS
+	- Katalysis BV (NL, kvk: 66264707)
 	### Contributors
 	$contributors
 	**Updating this list**
