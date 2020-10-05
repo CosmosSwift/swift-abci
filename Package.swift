41fb3fed98f27d75e6bd967853420e96bbcaa14d
Paulo Faria <paulo.faria.rl@gmail.com>
2020-10-05 17:00:29 -0300

    chore: Update Package.swift to Swift 5.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,14 +11,27 @@ let package = Package(
         .library(name: "ABCINIO", targets: ["ABCINIO"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.5.0"),
-        .package(url: "https://github.com/apple/swift-nio", from: "2.3.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.5.0"),
+        .package(name: "swift-nio", url: "https://github.com/apple/swift-nio", from: "2.3.0"),
+        .package(name: "swift-log", url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
         .target(name: "ABCICounter", dependencies: ["ABCI", "ABCINIO"]),
-        .target(name: "ABCI", dependencies: ["SwiftProtobuf", "Logging"]),
-        .target(name: "ABCINIO", dependencies: ["NIO", "Logging", "ABCI"]),
+        .target(
+            name: "ABCI",
+            dependencies: [
+                .product(name: "SwiftProtobuf", package: "SwiftProtobuf"),
+                .product(name: "Logging", package: "swift-log"),
+             ]
+        ),
+        .target(
+            name: "ABCINIO",
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "Logging", package: "swift-log"),
+                "ABCI"
+            ]
+        ),
         .testTarget(name: "ABCITests", dependencies: ["ABCI"]),
     ]
 )
